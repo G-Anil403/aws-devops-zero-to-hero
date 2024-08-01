18c6df341ead33ec9ef3da704afccba703c6b5b9
G-Anil403 <141538473+G-Anil403@users.noreply.github.com>
2024-08-02 00:40:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull ganil403/sample-python-flask
+docker pull ganil403/sample-python-flask
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ganil403/sample-python-flask
